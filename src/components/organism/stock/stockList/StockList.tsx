ae66b1b4956c362036f835557cd24f9bb62d296b
Stemps <kisn3089@naver.com>
2024-03-31 23:22:43 +0900

    [Feat]isLoading -> isFeching으로 수정

--- src/components/organism/stock/stockList/StockList.tsx
+++ src/components/organism/stock/stockList/StockList.tsx
@@ -10,24 +10,27 @@ import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
 const StockList = () => {
-  const { data: getStockList, isLoading } = useQuery<any[], AxiosError>({
+  const { data: getStockList, isFetching } = useQuery<any[], AxiosError>({
     queryKey: ["stockList"],
     enabled: false,
   });
-  console.log("getStockList: ", getStockList, isLoading);
+  console.log("getStockList: ", getStockList, isFetching);
 
   return (
     <StockListContainer>
-      {isLoading && <Loading>Loading...</Loading>}
-      {getStockList?.map((stock, i) => (
-        <StockItem key={i} onClick={() => console.log(stock)}>
-          {stock.itmsNm}
-          <PriceInfo $flag={Number(stock.fltRt) > 0 ? "up" : "down"}>
-            <span>{Number(stock.fltRt)}%</span>
-            <span>{Number(stock.clpr).toLocaleString("ko-KR")}</span>
-          </PriceInfo>
-        </StockItem>
-      ))}
+      {isFetching ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        getStockList?.map((stock, i) => (
+          <StockItem key={i} onClick={() => console.log(stock)}>
+            {stock.itmsNm}
+            <PriceInfo $flag={Number(stock.fltRt) > 0 ? "up" : "down"}>
+              <span>{Number(stock.fltRt)}%</span>
+              <span>{Number(stock.clpr).toLocaleString("ko-KR")}</span>
+            </PriceInfo>
+          </StockItem>
+        ))
+      )}
       <BottomLayout>
         <PagenationButton>이전</PagenationButton>
         <PagenationButton>다음</PagenationButton>

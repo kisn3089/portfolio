b5132d16c3f861847f72b62f1a42c1bef1fdb298
Stemps <kisn3089@naver.com>
2024-03-31 23:22:43 +0900

    [Feat]isLoading -> isFeching으로 수정

--- src/hooks/useGetStock.ts
+++ src/hooks/useGetStock.ts
@@ -4,8 +4,6 @@ import { AxiosError } from "axios";
 import React from "react";
 
 export const useGetStock = (search: string) => {
-  console.log("검색: ", search);
-
   return useQuery<any[], AxiosError>({
     queryKey: ["stockList"],
     queryFn: async () => await getStock(search.toLocaleUpperCase()),

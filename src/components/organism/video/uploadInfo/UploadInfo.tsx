8f96d5aeee62312f48b5dd59c7ee632bc983076f
Stemps <kisn3089@naver.com>
2024-04-06 02:16:01 +0900

    [Feat]프로젝트 페이지 반응형 & 비디오 프로젝트 반응형 적용

--- src/components/organism/video/uploadInfo/UploadInfo.tsx
+++ src/components/organism/video/uploadInfo/UploadInfo.tsx
@@ -2,16 +2,23 @@ import { InfoTitle, UploadInfoContainer } from "./UploadInfoStyles";
 
 const UploadInfo = () => {
   const content =
-    "Non-mp4 video files will be converted to mp4 extension and played back.";
+    "mp4 확장자가 아닌 비디오 파일은 mp4 확장자로 변환 후 동영상을 재생합니다.";
+  // "Non-mp4 video files will be converted to mp4 extension and played back.";
   // mp4 확장자가 아닌 비디오 파일은 mp4 확장자로 변환 후 동영상을 재생합니다.
   // 크기가 큰 파일은 피해주세요 🙏
   // 동작하지 않는다면 새로고침 후 이용해주세요. 👻
+  {
+    /* <li>Please avoid large files. 🙏</li> */
+  }
+  {
+    /* <li>If doesn't work, please refresh and try again. 👻</li> */
+  }
   return (
     <UploadInfoContainer>
       <InfoTitle>{content}</InfoTitle>
       <ul>
-        <li>Please avoid large files. 🙏</li>
-        <li>If doesn't work, please refresh and try again. 👻</li>
+        <li>크기가 큰 파일은 피해주세요 🙏</li>
+        <li>동작하지 않는다면 새로고침 후 이용해주세요. 👻</li>
       </ul>
     </UploadInfoContainer>
   );

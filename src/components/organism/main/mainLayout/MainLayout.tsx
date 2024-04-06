b75b7ff141f35de09f0d74e6a02bf53c3c7c7a69
Stemps <kisn3089@naver.com>
2024-04-07 00:03:07 +0900

    [Feat]반응형 모바일 오른쪽 여백 제거

---
+++ src/components/organism/main/mainLayout/MainLayout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { MainLayoutStyle } from "./MainLayoutStyles";
+
+const MainLayout = () => {
+  return <MainLayoutStyle>MainLayout</MainLayoutStyle>;
+};
+
+export default MainLayout;

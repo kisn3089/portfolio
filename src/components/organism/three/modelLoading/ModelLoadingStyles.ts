64c0b68c4d41c79771897b763c715d7dde409139
Stemps <kisn3089@naver.com>
2024-04-07 16:23:04 +0900

    [Feat]Homepage UI 구현

--- src/components/organism/three/modelLoading/ModelLoadingStyles.ts
+++ src/components/organism/three/modelLoading/ModelLoadingStyles.ts
@@ -1,7 +1,13 @@
+import { theme } from "@/styles/theme";
 import styled from "styled-components";
 
 export const ModelLoadContainer = styled.span<{ $bgColor: string }>`
   font-size: 56px;
   letter-spacing: 1.2px;
+  height: 700px;
   color: ${({ $bgColor }) => $bgColor};
+
+  @media screen and (max-width: ${theme.deviceSize.mobile}) {
+    height: 550px;
+  }
 `;

7c351674cb3b87752d4b10905743e3cdecea794e
Stemps <kisn3089@naver.com>
2024-04-08 23:49:29 +0900

    [Feat]Stock 반응형 구상 완료

--- src/components/organism/stock/stockBody/StockBodyStyle.ts
+++ src/components/organism/stock/stockBody/StockBodyStyle.ts
@@ -3,11 +3,21 @@ import styled from "styled-components";
 
 export const StockBodyContainer = styled.div`
   width: 100%;
-  height: 700px;
+  height: 640px;
   display: grid;
   grid-template-columns: 1fr 250px 250px;
+  border: ${({ theme }) => `1px solid ${theme.palette.gray}`};
+  border-radius: 8px;
 
   @media screen and (max-width: ${theme.deviceSize.desktop}) {
-    grid-template-columns: 1fr 180px 210px;
+    grid-template-columns: 1fr 180px 220px;
+  }
+
+  @media screen and (max-width: ${theme.deviceSize.tablet}) {
+    grid-template-columns: 1fr 220px;
+  }
+
+  @media screen and (max-width: ${theme.deviceSize.mobile}) {
+    grid-template-columns: 1fr;
   }
 `;
--- src/components/organism/stock/stockList/StockListStyle.ts
+++ src/components/organism/stock/stockList/StockListStyle.ts
@@ -4,6 +4,5 @@ export const StockListContainer = styled.div`
   position: relative;
   width: 100%;
   padding: 8px 8px;
-  border-radius: 0 8px 8px 0;
-  border: ${({ theme }) => `1px solid ${theme.palette.gray}`};
+  border-left: ${({ theme }) => `1px solid ${theme.palette.gray}`};
 `;

6a7ec9efb2a8e36bc7f653172550bf01f7e6e150
Stemps <kisn3089@naver.com>
2024-04-10 18:21:01 +0900

    [Refectoring]useSearch와 useStockList로 분리

--- src/pages/StockPage.tsx
+++ src/pages/StockPage.tsx
@@ -2,30 +2,29 @@ import StockBody from "@/components/organism/stock/stockBody/StockBody";
 import StockHeader from "@/components/organism/stock/stockHeader/StockHeader";
 import { StockSection } from "@/components/organism/stock/stockHeader/StockHeaderStyles";
 import useSearch from "@/hooks/useSearch";
+import useStockList from "@/hooks/useStockList";
 
 const StockPage = () => {
+  const { searchValue, changeSearch, closeClick, searchEnter } = useSearch();
   const {
     currentDate,
     fetchSearchValue,
-    searchValue,
     pagenation,
-    changeSearch,
-    searchEnter,
-    closeClick,
     footerClick,
     clickChangeDate,
-  } = useSearch();
+    enterCallback,
+  } = useStockList();
   return (
     <StockSection>
       <StockHeader
         searchValue={searchValue}
         changeSearch={changeSearch}
         searchEnter={searchEnter}
+        callback={enterCallback}
         closeClick={closeClick}
       />
       <StockBody
         currentDate={currentDate}
-        searchValue={searchValue}
         pagenation={pagenation}
         fetchSearchValue={fetchSearchValue}
         footerClick={footerClick}

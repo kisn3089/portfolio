ff60237e43cf52dbc443e53064ec3c9e0734f4b0
kisn3089 <kisn3089@naver.com>
2024-04-11 19:16:47 +0900

    [Feat]날자 변경시 주말 제외하는 자동화 로직 구현중

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -1,12 +1,28 @@
+import { adjusting, aviodHoliday } from "@/lib/util/adjustDate";
 import { calcById } from "@/lib/util/calcById";
 import React, { useState } from "react";
 
 const useDate = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
+
   const clickChangeDate = (e: React.MouseEvent) => {
     const { id } = e.currentTarget;
+    // 현재 일자를 요일로 변경
+    const currentDay = Math.floor((currentDate.getDate() + calcById(id)) % 7);
+    // 호출 가능한 요일로 변경
+    const adjustDate = adjusting[currentDay];
+    // console.log(currentDay);
+    // console.log(currentDay - adjustDate);
 
-    const newDate = currentDate.setDate(currentDate.getDate() + calcById(id));
+    console.log(currentDay);
+    console.log(adjustDate);
+    console.log(aviodHoliday[currentDay - adjustDate]);
+    const newDate = currentDate.setDate(
+      currentDate.getDate() +
+        calcById(id) -
+        aviodHoliday[currentDay - adjustDate]
+    );
+    console.log(new Date(newDate).getDay());
 
     setCurrentDate(new Date(newDate));
   };

c66976629e83563db9617df959764cc5ab3052f6
Stemps <kisn3089@naver.com>
2024-06-24 00:09:49 +0900

    [Feat]Image Sample_color layout 구현

--- src/components/organism/image/ImageSection.tsx
+++ src/components/organism/image/ImageSection.tsx
@@ -3,6 +3,7 @@ import {
   ImageLayout,
   InsideContent,
   InsideUpload,
+  SampleLayout,
   UploadLayout,
 } from "./ImageLayoutStyles";
 import * as Svg from "@/components/atoms/icon/index";
@@ -16,6 +17,10 @@ const ImageSection = () => {
           <InsideContent>Drag Down or Click</InsideContent>
         </InsideUpload>
       </UploadLayout>
+      <SampleLayout>
+        <img src="/assets/img/opera.webp" alt="sample image_color" />
+        <img src="/assets/img/opera.webp" alt="sample image_color" />
+      </SampleLayout>
     </ImageLayout>
   );
 };

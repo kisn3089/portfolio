ebac8bc836c88f5f2b1fe3ce86cce52793cd253e
kisn3089 <kisn3089@naver.com>
2024-06-24 18:42:02 +0900

    [Fix]upload & sample UI 변경

--- src/components/organism/image/ImageSection.tsx
+++ src/components/organism/image/ImageSection.tsx
@@ -1,25 +1,39 @@
-import React from "react";
+import { useImage } from "@/hooks/useImage";
 import {
+  BorderCenter,
+  GrayscaleImage,
   ImageLayout,
   InsideContent,
   InsideUpload,
+  LabelImage,
   SampleLayout,
+  UploadContainer,
   UploadLayout,
 } from "./ImageLayoutStyles";
 import * as Svg from "@/components/atoms/icon/index";
 
 const ImageSection = () => {
+  const { image, getImage } = useImage();
   return (
     <ImageLayout>
       <UploadLayout>
-        <InsideUpload>
-          <Svg.UploadImage />
-          <InsideContent>Drag Down or Click</InsideContent>
-        </InsideUpload>
+        <UploadContainer>
+          <BorderCenter />
+          <LabelImage htmlFor="image" />
+          <input
+            type="file"
+            id="image"
+            accept=".jpg, .png, .ico, .webp"
+            onChange={getImage}
+          />
+          <InsideUpload>
+            <Svg.UploadImage />
+            <InsideContent>Drag Down or Click</InsideContent>
+          </InsideUpload>
+        </UploadContainer>
       </UploadLayout>
       <SampleLayout>
-        <img src="/assets/img/opera.webp" alt="sample image_color" />
-        <img src="/assets/img/opera.webp" alt="sample image_color" />
+        <GrayscaleImage src="/assets/img/opera.webp" alt="image_color_custom" />
       </SampleLayout>
     </ImageLayout>
   );

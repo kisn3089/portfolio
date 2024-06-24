8bb528825da9a747537258e7a00e2357c291b5a5
Stemps <kisn3089@naver.com>
2024-06-25 00:05:40 +0900

    [Feat]upload UI 수정 & canvas drawImage 기능 구현 & upload image 변경시 canvas그림 지우게 수정해야할 과정

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -1,24 +1,32 @@
 import { imageProcessing } from "@/lib/util/imageProcessing";
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+type TImage = {
+  image: string;
+  canvasEl: HTMLCanvasElement | null;
+};
 
 export const useImage = () => {
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState<TImage>({ image: "", canvasEl: null });
+  const canvasRef = useRef<HTMLDivElement>(null);
   const getImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (files && files[0] && files[0].type.includes("image")) {
-      console.log(files[0].type);
       const url = URL.createObjectURL(files[0]);
-      setImage(url);
-      const image = new Image();
-      image.src = url;
-      image.width = 500;
-      image.height = 500;
-      imageProcessing(image);
+
+      const imageEl = new Image();
+      imageEl.src = url;
+      // imageEl.width = 500;
+      imageEl.height = 500;
+      imageEl.onload = () => {
+        const result = imageProcessing(imageEl);
+        setImage({ image: url, canvasEl: result });
+      };
     } else {
       // 이미지 타입이 아닐 경우
       alert("이미지 타입이 아닙니다");
       return;
     }
   };
-  return { image, getImage };
+  return { image, canvasRef, getImage };
 };

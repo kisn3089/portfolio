8f17a1d640fcb531b99b6b9fb22f23fdc7b23983
kisn3089 <kisn3089@naver.com>
2024-06-24 19:07:56 +0900

    [Feat]add imageprocessing.ts in util

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -1,3 +1,4 @@
+import { imageProcessing } from "@/lib/util/imageProcessing";
 import { useState } from "react";
 
 export const useImage = () => {
@@ -8,6 +9,11 @@ export const useImage = () => {
       console.log(files[0].type);
       const url = URL.createObjectURL(files[0]);
       setImage(url);
+      const image = new Image();
+      image.src = url;
+      image.width = 500;
+      image.height = 500;
+      imageProcessing(image);
     } else {
       // 이미지 타입이 아닐 경우
       alert("이미지 타입이 아닙니다");

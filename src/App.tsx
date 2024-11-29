6249b22bb7d9c5985cef8c622bf45bca685af861
Stemps <kisn3089@naver.com>
2024-11-29 15:59:22 +0900

    [Fix]About me 라우터 주석

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import AboutMePage from "./pages/AboutMePage";
+// import AboutMePage from "./pages/AboutMePage";
 import ProjectPage from "./pages/ProjectPage";
 import ImagePage from "./pages/ImagePage";
 import ThreePage from "./pages/ThreePage";
@@ -16,7 +16,7 @@ function App() {
       <CSSTransition key={location.pathname} classNames="fade" timeout={1000}>
         <Routes location={location}>
           <Route path="/" element={<MainPage />} />
-          <Route path="me" element={<AboutMePage />} />
+          {/* <Route path="me" element={<AboutMePage />} /> */}
           <Route path="project" element={<ProjectPage />} />
           <Route path="project/image" element={<ImagePage />} />
           <Route path="project/three" element={<ThreePage />} />
